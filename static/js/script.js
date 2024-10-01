34f475a45a659265e3fa0a1b13c3a7fa09c9e8bf
Mohit Rathod <57721715+mohitrathod7@users.noreply.github.com>
2024-10-01 00:39:32 -0400

    Delete static/js/script.js

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-function changeTeamImage(section) {
-    const selectElement = document.getElementById(section + '_name');
-    const selectedOption = selectElement.options[selectElement.selectedIndex];
-    const logoFileName = selectedOption.getAttribute('data-logo');
-    const logoElement = document.getElementById(section + '-logo');
-    
-    // Update the image source
-    logoElement.src = "{{ url_for('static', filename='img/team_logos/') }}" + logoFileName;
-}
